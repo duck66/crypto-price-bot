3f8294c17a0b9d7a0c34270cd249e942d582de61
Dhiva Marchandy <dhiva.hanifsyah@gmail.com>
2021-04-20 07:06:27 +0700

    Hide tele token

--- price_track.py
+++ price_track.py
@@ -2,10 +2,12 @@ import requests
 import telegram
 import json
 
+from telegram.forcereply import ForceReply
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
+from local_config import TOKEN
 
-telegram_bot_token = "THIS_IS_SECRET"
+telegram_bot_token = TOKEN
 
 updater = Updater(token=telegram_bot_token, use_context=True)
 dispatcher = updater.dispatcher
@@ -13,21 +15,28 @@ dispatcher = updater.dispatcher
 def rego(update, context):
     chat_id = update.effective_chat.id
 
+    pair = context.args[0]
+
     base_url = "https://indodax.com/api/ticker"
 
-    coin = f"{base_url}/{context.args[0]}idr"
+    coin = f"{base_url}/{pair}idr"
 
     response = requests.get(coin)
-
-    print(response.json())
 
     if response.json().get("ticker"):
         text = response.json().get("ticker").get("last")
         text = "{:0,}".format(float(text))
-        text = f"Regone Rp {text} cok!"
+        text = f"Regone {pair} Rp {text} cok!"
     else:
-        text = f"{context.args[0]} iku token apaan cok ?!"
-    context.bot.send_message(chat_id=chat_id, text=f"{text}")
+        text = f"{pair} iku token apaan cok ?!"
+    context.bot.send_message(chat_id=chat_id, text=text)
+
+def help(update, context):
+    chat_id = update.effective_chat.id
+    content = "rego+jeneng_pair nggo cek rego \n contoh : /rego doge"
+    context.bot.send_message(chat_id=chat_id, text=content)
 
 dispatcher.add_handler(CommandHandler("rego", rego))
+dispatcher.add_handler(CommandHandler("help", help))
+
 updater.start_polling()
